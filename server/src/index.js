ad68b7209befddff799923da396e8dbe40c24f70
unknown <08harianja@gmail.com>
2024-06-06 22:08:31 +0700

    feat: crud movies

---
+++ server/src/index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const bodyParser = require('body-parser');
+const fileUpload = require('express-fileupload');
+const movieRouter = require('./movie/movie.controller');
+const cors = require("cors");
+const path = require("path");
+
+const app = express();
+
+dotenv.config();
+
+const PORT = process.env.PORT;
+
+app.use('/images', express.static(path.join(__dirname, 'image')));
+
+
+app.use(express.json());
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(fileUpload());
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.send("Hello World dulu");
+});
+
+const moviesController = require("./movie/movie.controller");
+app.use("/movies", moviesController);
+
+app.use('/movies', movieRouter);
+
+app.listen(PORT, () => {
+    console.log("Express API running in port: "+ PORT);
+});
