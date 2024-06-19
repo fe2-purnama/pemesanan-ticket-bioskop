69e9bea85fc94a09465513aab18b0a06e5614821
trainerBE3 <163844685+trainerBE3@users.noreply.github.com>
2024-06-19 13:49:45 +0700

    Update db.js

--- backend/register-login-user/db.js
+++ backend/register-login-user/db.js
@@ -2,10 +2,11 @@
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'user'
+    host: 'educalab.id',
+    user: 'Z4iC4YbFydrLLuhf',
+    password: 'Yiralt1gifhO4DsI',
+    database: 'dDtlDrYwcMJcymx9',
+    port : 3307
 });
 
 db.connect((err) => {
